706100c3cc914ad328bbfc1187e0a42e2415a65e
Kaden Golda <kadengoldasteam@gmail.com>
2022-11-23 11:46:31 -0800

    Add files via upload
    Adjusting format of Repo

---
+++ admin.php
@@ -0,0 +1,45 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+};
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>login</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/components.css">
+
+</head>
+<body style="background-image: url('regback.jpg');">
+
+<section class="form-container">
+
+   <form action="" method="POST">
+      <h3>login now</h3>
+      <input type="email" name="email" class="box" placeholder="enter your email" required>
+      <input type="password" name="pass" class="box" placeholder="enter your password" required>
+      <input type="submit" value="login now" class="btn" name="submit">
+      <p>don't have an account? <a href="register.php">register now</a></p>
+   </form>
+
+</section>
+
+
+</body>
+</html>
